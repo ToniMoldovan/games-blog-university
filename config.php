a8898cee9610da12dc3517848e5c274310fbe8f9
ToniMoldovan <antonio.moldovan82@yahoo.com>
2022-11-23 23:57:24 +0200

    created config file

--- config.php
+++ config.php
@@ -1,3 +1,5 @@
 <?php
-
-$ROOT_PATH = "/home/stud/2022/tehweb/moldovan.akbar.antonio/public_html/"; //momentan
+session_start();
+//$_SESSION['logged'] = 'toni';
+define("ROOT_PATH", "https://davos.science.upm.ro/~2022.moldovan.akbar.antonio/");
+//$ROOT_PATH = "/home/stud/2022/tehweb/moldovan.akbar.antonio/public_html/"; //momentan
