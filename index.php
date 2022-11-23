704b22364297c8831e4ab08744f5131ab1244541
ToniMoldovan <antonio.moldovan82@yahoo.com>
2022-11-23 21:20:22 +0200

    created dynamic links

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php
-echo getcwd() . '/home';
+include_once 'config.php';
+global $ROOT_PATH;
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <title>Games Blog</title>
+</head>
+
+<body>
+
+<?php include $ROOT_PATH . 'include/components/navbar.php'; ?>
+
+<div id="test">
+    <p>hello</p>
+</div>
+
+<?php include $ROOT_PATH . 'include/components/footer.php'; ?>
+</body>
+</html>
