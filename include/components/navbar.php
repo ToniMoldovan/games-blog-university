f6e2845e413e98258b0f3741d97a3a3af771db23
ToniMoldovan <antonio.moldovan82@yahoo.com>
2022-11-26 21:48:38 +0200

    setup and customized navbar w/ logo & animations

--- include/components/navbar.php
+++ include/components/navbar.php
@@ -1,43 +1,45 @@
-<nav class="navbar navbar-expand-lg sticky-top">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+<nav id="my-nav" class="navbar navbar-expand-lg sticky-top">
+    <a class="navbar-brand" href="#">
+        <img src="<?php echo ROOT_PATH . 'assets/img/blog_logo_2.png';?>" width="200px" alt="blog_logo">
+    </a>
+    <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <div class="collapse navbar-collapse justify-content-lg-center justify-content-md-center justify-content-left" id="navbarNavDropdown">
         <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="index.php?page=home">Home</a>
+            <li class="nav-item">
+                <a class="nav-link hover-link" href="index.php?page=home">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="index.php?page=about">About</a>
+                <a class="nav-link hover-link" href="index.php?page=about">About</a>
             </li>
 
             <?php if (!isset($_SESSION['logged'])):  //Guest user menu ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?page=register">Register</a>
+                    <a class="nav-link hover-link" href="index.php?page=register">Register</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?page=login">Login</a>
+                    <a class="nav-link hover-link" href="index.php?page=login">Login</a>
                 </li>
             <?php endif;?>
 
             <?php if (isset($_SESSION['logged'])): //Logged user menu ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?page=account">My Account</a>
+                    <a class="nav-link hover-link" href="index.php?page=account">My Account</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?page=create_post">Create Post</a>
+                    <a class="nav-link hover-link" href="index.php?page=create_post">Create Post</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php?page=my_posts">My Posts</a>
+                    <a class="nav-link hover-link" href="index.php?page=my_posts">My Posts</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo ROOT_PATH.'include/functions/logout.php';?>">Logout</a>
+                    <a class="nav-link hover-link" href="<?php echo ROOT_PATH.'include/functions/logout.php';?>">Logout</a>
                 </li>
             <?php endif; ?>
 
             <li class="nav-item">
-                <a class="nav-link" href="index.php?page=contact">Contact</a>
+                <a class="nav-link hover-link" href="index.php?page=contact">Contact</a>
             </li>
         </ul>
     </div>
