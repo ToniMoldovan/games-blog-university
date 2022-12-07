2cdb5e179ad39b429ac5cd2719a31c74de1e0945
ToniMoldovan <antonio.moldovan82@yahoo.com>
2022-12-07 16:59:34 +0200

    improved logout function

--- gaming-blog/include/functions/logout.php
+++ gaming-blog/include/functions/logout.php
@@ -5,5 +5,6 @@ if (!isset($_SESSION['logged'])) {
     header("Location: https://davos.science.upm.ro/~2022.moldovan.akbar.antonio/");
 } else{
     session_unset();
+    session_destroy();
     header("Location: https://davos.science.upm.ro/~2022.moldovan.akbar.antonio/");
 }
