065a77ee4fe5758f39dbc82c82bdae35d58c1bb6
ToniMoldovan <antonio.moldovan82@yahoo.com>
2022-12-09 21:32:52 +0200

    added "create new post" functionality

--- gaming-blog/include/classes/Post.php
+++ gaming-blog/include/classes/Post.php
@@ -3,12 +3,12 @@
 require_once 'DB.php';
 
 class Post {
-    public string $table = 'posts';
+    public $table = 'posts';
 
-    private int $uid;
-    private string $title;
-    private string $content;
-    private string $img;
+    private $uid;
+    private $title;
+    private $content;
+    private $img;
 
     function __construct($title, $content, $img_url, $user_id) {
         $this->title = $title;
@@ -22,12 +22,39 @@ class Post {
 
     }
 
+    public static function postExists($post_id) {
+        $query = sprintf("SELECT * FROM posts WHERE id='%d' ", $post_id);
+
+        $result = DB::getInstance()->runQuery($query, "SELECT");
+        //post[0]['title']
+
+        if (count($result) != 1) {
+            $_SESSION['post_no_exist'] = 'This post ID ['.$post_id.'] doesn\'t exist. Please try again.';
+            header("location:" . ROOT_PATH . 'index.php?page=home');
+            return false;
+        }
+
+        return $result;
+    }
+
     public function store() {
-        //TODO: execute query
+        $timestamp = date("Y-m-d H:i:s");
+        $query = sprintf(
+            "INSERT INTO posts (user_id, title, content, image, created_at)
+                    VALUES ('%d', '%s', '%s', '%s', '%s');",
+            DB::escape($this->uid),
+            DB::escape($this->title),
+            DB::escape($this->content),
+            DB::escape($this->img),
+            DB::escape($timestamp));
+
+        DB::getInstance()->runQuery($query, 'INSERT');
+        $_SESSION['post_create_success'] = 'Post created successfully!';
+        header("location:" . ROOT_PATH . 'index.php?page=create_post');
     }
 
     /*Getters*/
-    public function getTable(): string
+    public function getTable()
     {
         return $this->table;
     }
@@ -53,27 +80,27 @@ class Post {
     }
 
     /*Setters*/
-    public function setTable(string $table): void
+    public function setTable(string $table)
     {
         $this->table = $table;
     }
 
-    public function setUid($uid): void
+    public function setUid($uid)
     {
         $this->uid = $uid;
     }
 
-    public function setTitle($title): void
+    public function setTitle($title)
     {
         $this->title = $title;
     }
 
-    public function setContent($content): void
+    public function setContent($content)
     {
         $this->content = $content;
     }
 
-    public function setImg($img): void
+    public function setImg($img)
     {
         $this->img = $img;
     }
