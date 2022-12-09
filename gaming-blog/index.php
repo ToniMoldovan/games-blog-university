d0c20bee1e559bb02e11d8c8b73d60794f0404ee
ToniMoldovan <antonio.moldovan82@yahoo.com>
2022-12-09 21:34:04 +0200

    improved routing switch

--- gaming-blog/index.php
+++ gaming-blog/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'config.php';
 require_once 'include/classes/DB.php';
+require_once 'include/classes/Post.php';
 ?>
 
 <!doctype html>
@@ -17,6 +18,8 @@ require_once 'include/classes/DB.php';
 <?php
 
 $page = isset($_GET['page']) ? $_GET['page'] : 'home';
+$post = isset($_GET['post_id']) ? $_GET['post_id'] : 0;
+
 switch ($page){
     case '':
     case 'home' :
@@ -24,11 +27,46 @@ switch ($page){
         break;
 
     case 'register':
-        include 'views/auth/register.php';
+        if (isLogged()) {
+            header("location:" . ROOT_PATH . 'index.php?page=home');
+        }
+        else {
+            include 'views/auth/register.php';
+        }
         break;
 
     case 'login':
-        include 'views/auth/login.php';
+        if (isLogged()) {
+            header("location:" . ROOT_PATH . 'index.php?page=home');
+        }
+        else {
+            include 'views/auth/login.php';
+        }
+        break;
+
+    case 'create_post':
+        if (isLogged()) {
+            include 'views/blog/store.php';
+        }
+        else {
+            header("location:" . ROOT_PATH . 'index.php?page=home');
+        }
+        break;
+
+    case 'post':
+        //echo $post;
+        if ($post < 1) {
+            $_SESSION['post_no_exist'] = 'This post id doesn\'t exist.';
+            header("location:" . ROOT_PATH . 'index.php?page=home');
+        }
+        else {
+            $requestedPost = Post::postExists($post);
+            if ($requestedPost !== false) {
+                include 'views/blog/post.php';
+                //var_dump($requestedPost);
+                //die;
+            }
+        }
         break;
 
     default:
@@ -42,5 +80,6 @@ switch ($page){
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </body>
 </html>
